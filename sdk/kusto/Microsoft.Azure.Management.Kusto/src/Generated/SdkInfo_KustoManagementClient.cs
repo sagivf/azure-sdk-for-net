a6f4a288c62b6571068841747a0a140ac24d5a58
Sagiv Frankel <safranke@microsoft.com>
2021-09-22 14:38:11 +0300

    wip

--- sdk/kusto/Microsoft.Azure.Management.Kusto/src/Generated/SdkInfo_KustoManagementClient.cs
+++ sdk/kusto/Microsoft.Azure.Management.Kusto/src/Generated/SdkInfo_KustoManagementClient.cs
@@ -34,5 +34,16 @@ namespace Microsoft.Azure.Management.Kusto
               }.AsEnumerable();
           }
       }
+      // BEGIN: Code Generation Metadata Section
+      public static readonly String AutoRestVersion = "v2";
+      public static readonly String AutoRestBootStrapperVersion = "autorest@2.0.4413";
+      public static readonly String AutoRestCmdExecuted = "cmd.exe /c autorest.cmd https://github.com/Azure/azure-rest-api-specs/blob/main/specification/azure-kusto/resource-manager/readme.md --csharp --version=v2 --reflect-api-versions --csharp-sdks-folder=C:\\dev\\sdks\\azure-sdk-for-net\\sdk";
+      public static readonly String GithubForkName = "Azure";
+      public static readonly String GithubBranchName = "main";
+      public static readonly String GithubCommidId = "2ecc4a457776feff5cf647d28c045ea9acffadb3";
+      public static readonly String CodeGenerationErrors = "";
+      public static readonly String GithubRepoName = "azure-rest-api-specs";
+      // END: Code Generation Metadata Section
   }
 }
+
